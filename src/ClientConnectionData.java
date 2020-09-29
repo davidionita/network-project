c6dfe8711452c83349823507ab2b10fc62ae0b30
David Ionita <david@ionita.dev>
2020-09-29 14:58:19 -0400

    initial commit

---
+++ src/ClientConnectionData.java
@@ -0,0 +1,61 @@
+package day5_bca;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientConnectionData {
+
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    private String name;
+    private String userName;
+
+    public ClientConnectionData(Socket socket, BufferedReader in, PrintWriter out, String name) {
+        this.socket = socket;
+        this.in = in;
+        this.out = out;
+        this.name = name;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public BufferedReader getIn() {
+        return in;
+    }
+
+    public void setIn(BufferedReader in) {
+        this.in = in;
+    }
+
+    public PrintWriter getOut() {
+        return out;
+    }
+
+    public void setOut(PrintWriter out) {
+        this.out = out;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
